df10c7aaab55471e6fedf1c511870d65c391d2e0
Anik Roy <anikroy.uiu@gmail.com>
2025-08-16 19:50:45 +0600

    changes

--- src/components/sections/navbar/Navbar.jsx
+++ src/components/sections/navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Container from '../../layers/Container'
+import Image from '../../layers/Image'
 
 const Navbar = () => {
   return (
     <Container>
-      
+     <div>
+        <div>
+          <Image imgsrc="./images/logo.png"/>
+        </div>
+        <div>
+          <div>
+
+          </div>
+        </div>
+        <div>
+          
+        </div>
+     </div>
     </Container>
   )
 }
