66044e0cbfa9549a276b0e1359311d91e302d091
Anik Roy <anikroy.uiu@gmail.com>
2025-08-22 22:53:50 +0600

    Navbar and Container changes

--- src/components/sections/navbar/Navbar.jsx
+++ src/components/sections/navbar/Navbar.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import Container from '../../layers/Container'
 import Image from '../../layers/Image'
+import List from '../../layers/List'
+import ListItem from '../../layers/ListItem'
 
 const Navbar = () => {
   return (
     <Container>
-     <div>
+     <div className='flex justify-between items-center'>
         <div>
           <Image imgsrc="./images/logo.png"/>
         </div>
-        <div>
-          <div>
+        <div className=''>
+          <List className='flex gap-x-10'>
+            <ListItem  ListInnerItem="Home" href="/"/>
+            <ListItem  ListInnerItem="About Us" href="/about"/>
+            <ListItem  ListInnerItem="Services" href="/services"/>
+            <ListItem  ListInnerItem="Events" href="/contact"/>
+            <ListItem  ListInnerItem="Contact Us" href="/blog"/> 
+          </List>
 
-          </div>
         </div>
         <div>
-          
+           <div className='flex items-center justify-center gap-x-10'>
+            <button className="relative after:absolute after:w-[1px] after:h-[32px] after:bg-[#BFBFBF] after:right-[-23px] after:top-[50%] after:-translate-y-[50%] py-[10px] px-[10px] bg-[#305A7C] border border-solid border-[#CDCDCD] rounded-[10px] text-[#FEFFFE] font-medium text-base" >
+              Login
+            </button>
+            <button className='py-[10px] px-[10px] bg-[#305A7C] border border-solid border-[#CDCDCD] rounded-[10px] text-[#FEFFFE] font-medium text-base '>
+              Sign Up
+            </button>
+            
+           </div>
+           <div>
+              <button className="">Donate</button>
+           </div>
         </div>
      </div>
 
