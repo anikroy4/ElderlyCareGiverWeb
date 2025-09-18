c89397c589fa62fc778058473578ce24e3da559b
Anik Roy <anikroy.uiu@gmail.com>
2025-09-19 00:55:40 +0600

    Update Navbar.jsx

--- src/components/sections/navbar/Navbar.jsx
+++ src/components/sections/navbar/Navbar.jsx
@@ -23,16 +23,16 @@ const Navbar = () => {
         </div>
         <div className='flex gap-x-10 items-center justify-center'>
            <div className='flex items-center justify-center gap-x-10'>
-            <button className="relative after:absolute after:w-[1px] after:h-[32px] after:bg-[#BFBFBF] after:right-[-23px] after:top-[50%] after:-translate-y-[50%] py-[10px] px-[10px] bg-[#305A7C] border border-solid border-[#CDCDCD] rounded-[10px] text-[#FEFFFE] font-medium text-base" >
+            <button className="relative after:absolute after:w-[1px] after:h-[32px] after:bg-[#BFBFBF] after:right-[-23px] after:top-[50%] after:-translate-y-[50%] py-[4px] px-[10px] bg-[#305A7C] border border-solid border-[#CDCDCD] rounded-[10px] text-[#FEFFFE] font-medium text-base" >
               Login
             </button>
-            <button className='py-[10px] px-[10px] bg-[#305A7C] border border-solid border-[#CDCDCD] rounded-[10px] text-[#FEFFFE] font-medium text-base '>
+            <button className='py-[4px] px-[10px] bg-[#305A7C] border border-solid border-[#CDCDCD] rounded-[10px] text-[#FEFFFE] font-medium text-base '>
               Sign Up
             </button>
             
            </div>
            <div>
-              <button className="">Donate</button>
+              <button className=" py-[4px] px-[10px] rounded-[10px] border border-solid text-[#FEFFFE] font-medium text-base bg-[#2E95E9]">Donate</button>
            </div>
         </div>
      </div>
